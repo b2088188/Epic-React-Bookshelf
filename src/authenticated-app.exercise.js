9ca778c7aac2982afa563e78bfdfb0dd518710cf
Shunze Lin <4A490101@stust.edu.tw>
2021-01-31 13:22:28 +0800

    Routing exercise extra-1 finished

--- src/authenticated-app.exercise.js
+++ src/authenticated-app.exercise.js
@@ -2,13 +2,17 @@
 import {jsx} from '@emotion/core'
 
 import * as React from 'react'
-// We'll be doing a lot of stuff with the router on this page.
-// 🐨 Here's what you'll need to import from react-router-dom
-// Routes, Route, Link
+import {Routes, Route, useParams, Link} from 'react-router-dom'
+import {BookScreen} from './screens/book.exercise'
+import {DiscoverBooksScreen} from './screens/discover'
+import {NotFoundScreen} from './screens/not-found.exercise'
 import {Button} from './components/lib'
 import * as mq from './styles/media-queries'
 import * as colors from './styles/colors'
 // 🐨 you'll need to import all the screen components in the screens directory
+// /discover       ->     Discover books screen
+// /book/:bookId   ->     Book screen
+// *               ->     Helpful "not found" screen
 // 💰 DiscoverBooksScreen, BookScreen, NotFoundScreen
 
 function AuthenticatedApp({user, logout}) {
@@ -56,9 +60,8 @@ function AuthenticatedApp({user, logout}) {
 }
 
 function NavLink(props) {
-  // 🐨 change this from an <a /> to a <Link />
   return (
-    <a
+    <Link
       css={{
         display: 'block',
         padding: '8px 15px 8px 10px',
@@ -101,11 +104,7 @@ function Nav() {
         }}
       >
         <li>
-          {/*
-              🐨 Once the NavLink has been updated to use a Router Link,
-                change from the href prop to a "to" prop
-          */}
-          <NavLink href="/discover">Discover</NavLink>
+          <NavLink to="/discover">Discover</NavLink>
         </li>
       </ul>
     </nav>
@@ -113,14 +112,13 @@ function Nav() {
 }
 
 function AppRoutes({user}) {
-  // 🐨 Return all the routes here.
-  // 💰 Here's the mapping of URL to element:
-  //     /discover         <DiscoverBooksScreen user={user} />
-  //     /book/:bookId     <BookScreen user={user} />
-  //     *                 <NotFoundScreen />
-  //
-  // Make sure to check the INSTRUCTIONS.md for how this should be structured
-  return null
+  return (
+    <Routes>
+      <Route path="/discover" element={<DiscoverBooksScreen user={user} />} />
+      <Route path="/book/:bookId" element={<BookScreen user={user} />} />
+      <Route path="*" element={<NotFoundScreen />} />
+    </Routes>
+  )
 }
 
 export {AuthenticatedApp}
--- src/setupProxy.exercise.js
+++ src/setupProxy.exercise.js
@@ -1,4 +1,4 @@
-// 🐨 you don't need to do anything in this file for the exercise. This is
-// just here for the extra credit. See the instructions for more info.
-
-module.exports = () => {}
+// Redirect by express
+module.exports = app => {
+	app.get(/^\/$/, (req, res) => res.redirect('/discover'))
+}
